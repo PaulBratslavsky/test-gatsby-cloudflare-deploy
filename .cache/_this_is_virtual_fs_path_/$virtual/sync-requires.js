04d6faaa1f0314400f548fbb61abb207723c9150
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2022-12-08 15:32:16 -0600

    initial commit

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/paulbratslavsky/Desktop/wfc/cloudflare/gatsby-test/frontend/.cache/dev-404-page.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/paulbratslavsky/Desktop/wfc/cloudflare/gatsby-test/frontend/src/pages/about.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/paulbratslavsky/Desktop/wfc/cloudflare/gatsby-test/frontend/src/pages/index.js")),
+  "component---src-templates-article-post-js": preferDefault(require("/Users/paulbratslavsky/Desktop/wfc/cloudflare/gatsby-test/frontend/src/templates/article-post.js"))
+}
+
